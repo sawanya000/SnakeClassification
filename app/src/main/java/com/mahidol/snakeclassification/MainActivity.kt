fde115f458589b3da211ec1faeb0b2e6ffb7900e
webassignment <gifeminum@gmail.com>
2019-10-10 13:45:21 +0700

    popup ด้านล่าง

--- app/src/main/java/com/mahidol/snakeclassification/MainActivity.kt
+++ app/src/main/java/com/mahidol/snakeclassification/MainActivity.kt
@@ -2,11 +2,19 @@ package com.mahidol.snakeclassification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonopenbottomsheet.setOnClickListener {
+            val view = layoutInflater.inflate(R.layout.bottom_sheet_layout,null)
+            val dialog = BottomSheetDialog(this)
+            dialog.setContentView(view)
+            dialog.show()
+        }
     }
 }
