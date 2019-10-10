a41c65b0612d89b1a37fa6f636cd7bb133813fb1
sawanya000 <sawanya000@gmail.com>
2019-10-10 14:28:12 +0700

    add implementaion

--- app/src/main/java/com/mahidol/snakeclassification/MainActivity.kt
+++ app/src/main/java/com/mahidol/snakeclassification/MainActivity.kt
@@ -3,11 +3,11 @@ package com.mahidol.snakeclassification
 import android.content.Context
 
 import android.os.Bundle
-import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import kotlinx.android.synthetic.main.activity_main.*
+
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -19,26 +19,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        buttonopenbottomsheet.setOnClickListener {
-            val view = layoutInflater.inflate(R.layout.bottom_sheet_layout,null)
+        setButton()
+        setButtonSwitchLanguage()
+    }
+
+    private fun setButton() {
+        pictureBtn.setOnClickListener {
+            val view = layoutInflater.inflate(R.layout.bottom_sheet_layout, null)
             val dialog = BottomSheetDialog(this)
             dialog.setContentView(view)
             dialog.show()
         }
-        setButtonSwitchLanguage()
     }
 
-    private fun setButtonSwitchLanguage(){
-        val currentLanguage = LocaleHelper().getPersistedData(this,"en")
+    private fun setButtonSwitchLanguage() {
+        val currentLanguage = LocaleHelper().getPersistedData(this, "en")
         btn_sw_lan.isChecked = currentLanguage == "th"
         btn_sw_lan.setOnCheckedChangeListener { compoundButton, isChecked ->
             /*
                 true = th
                 false = en
             */
-            if (isChecked){
+            if (isChecked) {
                 setLocaleLanguage("th")
-            }else{
+            } else {
                 setLocaleLanguage("en")
             }
         }
