6cdad109517285901bb178ab68bce7cd01cf476a
sawanya000 <sawanya000@gmail.com>
2019-10-10 14:47:13 +0700

    add info activity

---
+++ app/src/main/java/com/mahidol/snakeclassification/SnakeInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.mahidol.snakeclassification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SnakeInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_snake_info)
+    }
+}
