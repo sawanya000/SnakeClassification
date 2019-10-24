7479bc7876d69141fc4b8ebcdde3c453822438a1
sawanya000 <sawanya000@gmail.com>
2019-10-24 16:21:34 +0700

    finish result page

--- app/src/main/java/com/mahidol/snakeclassification/MainActivity.kt
+++ app/src/main/java/com/mahidol/snakeclassification/MainActivity.kt
@@ -23,6 +23,20 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun setInfoButton() {
+        infoBtn.setOnClickListener {
+            val intent = Intent(this,SnakeInfoActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun setManualButton() {
+        manualBtn.setOnClickListener {
+            val intent = Intent(this,ManualActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
     private fun setButton() {
         pictureBtn.setOnClickListener {
 //            val view = layoutInflater.inflate(R.layout.bottom_sheet_layout, null)
@@ -54,6 +68,10 @@ class MainActivity : AppCompatActivity() {
     private fun setLocaleLanguage(language: String) {
         val context = LocaleHelper().setLocal(this, language)
         val resources = context.resources
+
+        textView2.text = resources.getString(R.string.Main_ChoosePic)
+        textView3.text = resources.getString(R.string.Main_Info)
+        textView4.text = resources.getString(R.string.Main_Manual)
 
         /* change this code follow your id text
         textView.text = resources.getString(R.string.Main_Info)
--- app/src/main/java/com/mahidol/snakeclassification/RecyclerResultAdapter.kt
+++ app/src/main/java/com/mahidol/snakeclassification/RecyclerResultAdapter.kt
@@ -2,121 +2,75 @@ package com.mahidol.snakeclassification
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.cardview.widget.CardView
+import androidx.core.view.isInvisible
 import androidx.recyclerview.widget.RecyclerView
-import com.mahidol.snakeclassification.Interface.ItemClickListener
 
-class RecyclerResultViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+class RecyclerResultViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
-    var headerTxt: TextView
-    var introTxt: TextView
-    var image_left: ImageView
-    var image_right: ImageView
-    var layoutTxt: ConstraintLayout
-    var backgroundCard: ConstraintLayout
-    private var itemClickListener: ItemClickListener? = null
-
+    var speciesTxt: TextView
+    var serumTxt: TextView
+    var probabilityPercent: TextView
+    var colorResult: CardView
+    var line:View
 
     init {
-        image_left = itemView.findViewById(R.id.snakeImgLeft)
-        image_right = itemView.findViewById(R.id.snakeImgRight)
-        backgroundCard = itemView.findViewById(R.id.backgroundCard)
-        layoutTxt = itemView.findViewById(R.id.layout_text)
-        headerTxt = itemView.findViewById(R.id.headerTxt)
-        introTxt = itemView.findViewById(R.id.introTxt)
-        introTxt.setOnClickListener(this)
-
+        speciesTxt = itemView.findViewById(R.id.speciesTxt)
+        serumTxt = itemView.findViewById(R.id.serumTxt)
+        probabilityPercent = itemView.findViewById(R.id.probabilityPercent)
+        colorResult = itemView.findViewById(R.id.colorResult)
+        line = itemView.findViewById(R.id.line)
     }
 
-    override fun onClick(v: View?) {
-        itemClickListener!!.onClick(v, adapterPosition, false)
-    }
+
 }
 
-class RecyclerResultAdapter(private val infoData: ArrayList<InfoData>, private val mContext: Context) :
+class RecyclerResultAdapter(
+    private val resultData: ArrayList<ResultData>,
+    private val mContext: Context
+) :
     RecyclerView.Adapter<RecyclerResultViewHolder>() {
 
     private val inflater: LayoutInflater
-    private val marginLayout: Int
 
     init {
         inflater = LayoutInflater.from(mContext)
-        marginLayout = 60
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerResultViewHolder {
 
-        val itemView = inflater.inflate(R.layout.recycler_card, parent, false)
+        val itemView = inflater.inflate(R.layout.recycler_result, parent, false)
         return RecyclerResultViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: RecyclerResultViewHolder, position: Int) {
 
-        val params_layoutTxt = holder.layoutTxt.layoutParams as ConstraintLayout.LayoutParams
-        holder.headerTxt.text = infoData[position].header
-        holder.introTxt.text = infoData[position].intro
-
-        when (position % 2) {
-            0 -> {
-
-                setImageInCard(holder.image_left, infoData[position].image)
-                setVisibleDisappear(holder.image_right)
-                setParamsToBgCard(holder.backgroundCard, "left")
-                params_layoutTxt.leftToRight = holder.image_left.id
-            }
-            1 -> {
-                setImageInCard(holder.image_right, infoData[position].image)
-                setVisibleDisappear(holder.image_left)
-                setParamsToBgCard(holder.backgroundCard, "right")
-                params_layoutTxt.rightToLeft = holder.image_right.id
+        holder.speciesTxt.text = resultData[position].species
+        holder.serumTxt.text = "เซรุ่ม : "
+        holder.probabilityPercent.text = "${resultData[position].probability}%"
+        var color = 0
+        when(position%4){
+            0 -> color = Color.parseColor("#E07A5F")
+            1 -> color = Color.parseColor("#F2CC8F")
+            2 -> color = Color.parseColor("#81B29A")
+            3 -> {
+                color = Color.parseColor("#E9E9E9")
+                holder.line.visibility = View.GONE
+                holder.serumTxt.visibility = View.GONE
             }
         }
-
-        setOnClickLayout(holder.introTxt)
+        holder.colorResult.setCardBackgroundColor(color)
 
     }
 
-    private fun setParamsToBgCard(backgroundCard: ConstraintLayout, show: String) {
-        val params_backgroundCard = ConstraintLayout.LayoutParams(
-            ConstraintLayout.LayoutParams.MATCH_PARENT,
-            ConstraintLayout.LayoutParams.MATCH_PARENT
-        )
-        if (show == "left")
-            params_backgroundCard.setMargins(marginLayout, 0, 0, 0)
-        else
-            params_backgroundCard.setMargins(0, 0, marginLayout, 0)
-        backgroundCard.layoutParams = params_backgroundCard
-    }
-
-    private fun setVisibleDisappear(view: View) {
-        view.visibility = View.GONE
-    }
-
-    private fun setImageInCard(imageView: ImageView, image: Int) {
-        imageView.setImageResource(image)
-    }
-
-    private fun setOnClickLayout(layoutTxt: TextView) {
-        layoutTxt.setOnClickListener {
-            openSaovabhaLink()
-        }
-    }
-
-    private fun openSaovabhaLink() {
-        val browserIntent =
-            Intent(Intent.ACTION_VIEW, Uri.parse("https://www.saovabha.com/th/snakefarm.asp"))
-        browserIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        mContext.startActivity(browserIntent)
-    }
 
     override fun getItemCount(): Int {
-        return infoData.size
+        return resultData.size
     }
 
 }
