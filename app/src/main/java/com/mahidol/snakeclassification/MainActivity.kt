8b6a2edd65203d96be337085621067052a9673e3
sawanya000 <sawanya000@gmail.com>
2019-10-30 17:16:24 +0700

    set function เปลี่ยนภาษาทุกหน้า(ยกเว้น หน้า result)

--- app/src/main/java/com/mahidol/snakeclassification/MainActivity.kt
+++ app/src/main/java/com/mahidol/snakeclassification/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.mahidol.snakeclassification
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.theartofdev.edmodo.cropper.CropImage
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,36 +19,58 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setButton()
+        setPictureButton()
         setInfoButton()
         setButtonSwitchLanguage()
         setManualButton()
 
     }
 
+    private fun onChooseFile() {
+        CropImage.activity().start(this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            var result :CropImage.ActivityResult? = CropImage.getActivityResult(data)
+
+            if (resultCode == Activity.RESULT_OK){
+                /*
+                    imageUri = result!!.uri
+                    imageView.setImageURI(imageUri)
+                */
+                val intent = Intent(this,ResultActivity::class.java)
+                startActivity(intent)
+            }
+            else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE){
+                var e: Exception = result!!.error
+                Toast.makeText(this,"Error is: $e", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
     private fun setInfoButton() {
         infoBtn.setOnClickListener {
-            val intent = Intent(this,SnakeInfoActivity::class.java)
+            val intent = Intent(this, SnakeInfoActivity::class.java)
             startActivity(intent)
         }
     }
 
     private fun setManualButton() {
         manualBtn.setOnClickListener {
-            val intent = Intent(this,ManualActivity::class.java)
+            val intent = Intent(this, ManualActivity::class.java)
             startActivity(intent)
         }
     }
 
-    private fun setButton() {
+    private fun setPictureButton() {
         pictureBtn.setOnClickListener {
-//            val view = layoutInflater.inflate(R.layout.bottom_sheet_layout, null)
-//            val dialog = BottomSheetDialog(this)
-//            dialog.setContentView(view)
-//            dialog.show()
+            onChooseFile()
 
-            val intent = Intent(this,ResultActivity::class.java)
-            startActivity(intent)
+//            val intent = Intent(this, ResultActivity::class.java)
+//            startActivity(intent)
         }
     }
 
@@ -65,6 +90,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+        val currentLanguage = LocaleHelper().getPersistedData(this, "en")
+        btn_sw_lan.isChecked = currentLanguage == "th"
+    }
+
     private fun setLocaleLanguage(language: String) {
         val context = LocaleHelper().setLocal(this, language)
         val resources = context.resources
@@ -72,7 +103,7 @@ class MainActivity : AppCompatActivity() {
         textView2.text = resources.getString(R.string.Main_ChoosePic)
         textView3.text = resources.getString(R.string.Main_Info)
         textView4.text = resources.getString(R.string.Main_Manual)
-
+        textView5.text = resources.getString(R.string.Main_History)
         /* change this code follow your id text
         textView.text = resources.getString(R.string.Main_Info)
         textView5.text = resources.getString(R.string.Main_Manual)*/
--- app/src/main/java/com/mahidol/snakeclassification/SnakeInfoActivity.kt
+++ app/src/main/java/com/mahidol/snakeclassification/SnakeInfoActivity.kt
@@ -1,5 +1,7 @@
 package com.mahidol.snakeclassification
 
+import android.content.Context
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -7,10 +9,15 @@ import kotlinx.android.synthetic.main.activity_snake_info.*
 
 class SnakeInfoActivity : AppCompatActivity() {
     private lateinit var data: ArrayList<InfoData>
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(LocaleHelper().onAttach(newBase!!, "en"))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_snake_info)
-        setInfoDataSnake()
+        setLanguage()
         setUpRecyclerView()
     }
 
@@ -24,48 +31,60 @@ class SnakeInfoActivity : AppCompatActivity() {
         recycler_view.adapter = adapter
     }
 
-    private fun setInfoDataSnake() {
+    private fun setInfoDataSnake(resources:Resources) {
         data = arrayListOf(
             InfoData(
                 R.mipmap.king_cobra_img,
-                getStringFromRes(R.string.King_Cobra),
-                getStringFromRes(R.string.Info_King_Cobra)
+                resources.getString(R.string.King_Cobra),
+                resources.getString(R.string.Info_King_Cobra)
             ),
             InfoData(
                 R.mipmap.cobra_img,
-                getStringFromRes(R.string.Cobra),
-                getStringFromRes(R.string.Info_Cobra)
+                resources.getString(R.string.Cobra),
+                resources.getString(R.string.Info_Cobra)
             ),
             InfoData(
                 R.mipmap.banded_krait_img,
-                getStringFromRes(R.string.Banded_Krait),
-                getStringFromRes(R.string.Info_Banded_Krait)
+                resources.getString(R.string.Banded_Krait),
+                resources.getString(R.string.Info_Banded_Krait)
             ),
             InfoData(
                 R.mipmap.malayan_krait_img,
-                getStringFromRes(R.string.Malayan_Krait),
-                getStringFromRes(R.string.Info_Malayan_Krait)
+                resources.getString(R.string.Malayan_Krait),
+                resources.getString(R.string.Info_Malayan_Krait)
             ),
             InfoData(
                 R.mipmap.russell_viper_img,
-                getStringFromRes(R.string.Russell_Viper),
-                getStringFromRes(R.string.Info_Russell_Viper)
+                resources.getString(R.string.Russell_Viper),
+                resources.getString(R.string.Info_Russell_Viper)
             ),
             InfoData(
                 R.mipmap.malayan_pit_viper_img,
-                getStringFromRes(R.string.Malayan_Pitviper),
-                getStringFromRes(R.string.Info_Malayan_Pitviper)
+                resources.getString(R.string.Malayan_Pitviper),
+                resources.getString(R.string.Info_Malayan_Pitviper)
             ),
             InfoData(
                 R.mipmap.white_lipped_pit_viper_img,
-                getStringFromRes(R.string.White_lipped_Pitviper),
-                getStringFromRes(R.string.Info_White_lipped_Pitviper)
+                resources.getString(R.string.White_lipped_Pitviper),
+                resources.getString(R.string.Info_White_lipped_Pitviper)
             )
 
         )
     }
 
-    private fun getStringFromRes(string: Int): String {
-        return resources.getString(string)
+    private fun setLanguage() {
+        val currentLanguage = LocaleHelper().getPersistedData(this, "en")
+        if (currentLanguage == "th") {
+            setLocaleLanguage("th")
+        } else {
+            setLocaleLanguage("en")
+        }
     }
+
+    private fun setLocaleLanguage(language: String) {
+        val context = LocaleHelper().setLocal(this, language)
+        val resources = context.resources
+        setInfoDataSnake(resources)
+    }
+
 }
